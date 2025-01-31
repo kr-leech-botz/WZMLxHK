7b0b685941626dcbdcb51e2ae7b1acf4addd859f
kr-leech-botz <120879618+kr-leech-botz@users.noreply.github.com>
2025-01-31 12:30:32 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -3,7 +3,7 @@ class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = 'Group'
-    ST_BN1_URL = 'https://t.me/free_leech_group'
+    ST_BN1_URL = 'https://t.me/Free_4GB_Leech_Group'
     ST_BN2_NAME = 'Updates'
     ST_BN2_URL = 'https://t.me/Srilinks4k_In'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
